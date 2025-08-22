7138252a62cd6c8ec13fc1b83d73c52d93f45410
gs0428 <lider28@naver.com>
2025-08-22 11:25:14 +0900

    feat: 전체화면 기능 추가

--- apps/web/src/components/(with-side-bar)/library/content-sidebar.tsx
+++ apps/web/src/components/(with-side-bar)/library/content-sidebar.tsx
@@ -339,7 +339,7 @@ export default function ContentSidebar() {
                   className="aspect-video"
                   src={`https://www.youtube.com/embed/${youtubeId}`}
                   title="YouTube video player"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
                 />
               )}
 

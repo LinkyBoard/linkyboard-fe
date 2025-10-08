da383fdc263f8eca5b174ccb9da606745f28a5b5
gs0428 <lider28@naver.com>
2025-10-08 12:20:40 +0900

    chore: 사용하지 않는 link 태그 제거

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -78,10 +78,6 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Merriweather:wght@300;400;700&family=JetBrains+Mono:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body className={cn(poppins.className, "antialiased")}>
         <ThirdPartyProvider />
